5c093ce0e998638c384a0b5e5ac9752b21d9a49f
Yogesh Bhagwat <bhagwatyogesh97@gmail.com>
2024-07-20 19:47:54 +0530

    Add React folder.

---
+++ Basic React/App.js
@@ -0,0 +1,3 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let h1 = React.createElement("h1", {id: "box"}, "Yogesh Bhagwat");
+root.render(h1);
---
+++ BasicHTML/App.js
@@ -0,0 +1,8 @@
+let body = document.querySelector("body");
+let root = document.createElement("root");
+body.appendChild(root);
+
+
+let h1 = document.createElement("h1");
+h1.textContent = "Yogesh Bhagwat";
+root.appendChild(h1);
